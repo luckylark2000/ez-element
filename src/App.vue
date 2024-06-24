b7ccdd666bc761db21b21195301c57a5ce2003c9
luckylark2000 <3286481191@qq.com>
2024-06-24 15:21:13 +0800

    project init, Button component done:part of props and type, basic use

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import Button from './components/Button/Button.vue';
+import type { ButtonInstance } from './components/Button/types';
+
+const btnRef = ref<ButtonInstance | null>(null);
+onMounted(() => {
+  console.log(btnRef.value?.ref);
+});
+</script>
+
+<template>
+  <main>
+    <Button ref="btnRef" type="primary" plain disabled native-type="button">test</Button>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    ref="_ref"
+    class="ez-button"
+    :class="{
+      [`ez-button--${type}`]: type,
+      [`ez-button--${size}`]: size,
+      'is-plain': plain,
+      'is-disabled': disabled,
+      'is-loading': loading,
+      'is-round': round,
+      'is-circle': circle,
+    }"
+    :disabled="disabled || loading"
+    :type="nativeType"
+    :autofocus="autofocus"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { type ButtonProps } from './types';
+
+defineOptions({ name: 'EzButton' });
+withDefaults(defineProps<ButtonProps>(), {
+  size: 'normal',
+  type: 'primary',
+  circle: false,
+  round: false,
+  plain: false,
+  disabled: false,
+  loading: false,
+  nativeType: 'button',
+  autofocus: false,
+});
+
+const _ref = ref<HTMLButtonElement | null>(null);
+
+defineExpose({
+  ref: _ref,
+});
+</script>
+
+<style scoped></style>
