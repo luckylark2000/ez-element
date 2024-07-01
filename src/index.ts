7d3afd972505adc97394f9d4164a2d258074ff75
luckylark2000 <3286481191@qq.com>
2024-07-01 15:16:25 +0800

    把导出的css文件转移到dist根目录，解决在外部使用时出现找不到样式的错误

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { App } from 'vue';
+import type { App, Plugin } from 'vue';
 import Button from '@/components/Button';
 import Collapse, { CollapseItem } from '@/components/Collapse';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -10,7 +10,7 @@ library.add(fas);
 
 const components = [Button, Collapse, CollapseItem];
 
-const installComponents = {
+const installComponents: Plugin = {
   install(app: App) {
     components.forEach((component) => {
       app.component(component.name as string, component);
