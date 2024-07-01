d611b05a011a4e03ffb9a54b434a7809d75a6501
luckylark2000 <3286481191@qq.com>
2024-07-01 18:04:40 +0800

    更新vitepress的base配置为ez-element

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'Ez Element',
+  base: '/ez-element/',
   description: 'A Vue3 UI components library',
   vite: {
     resolve: {
